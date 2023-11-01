dc0746b9361a6ef08e349abb73ed1d6abcd83717
Asmasaiyed104 <97555221+Asmasaiyed104@users.noreply.github.com>
2023-11-01 17:52:26 +0000

    Update settings.json <!-- This repository *does not* accept pull requests (PRs). All pull requests will be closed. See CONTRIBUTING.md for further details. -->

--- _01_02e/Employee.java
+++ _01_02e/Employee.java
@@ -5,37 +5,53 @@ public class Employee {
   public static void main(String[] args) {
 
     // Create a variable called age of type int and assign it the value 29.
-    int age = 29;
+    int age; // Declare the variable 'age' of type int
+    age = 29; // Assign the value 29 to the 'age' variable
 
     // Print the age variable to the console.
     System.out.println(age);
 
     // Create a variable called isAManager of type boolean and assign it the value
     // true.
+    boolean isAManager = true;
 
     // Print the isAManager variable to the console.
+    System.out.print(isAManager);
 
     // Create a variable called yearsOfService of type double and assign it the
     // value 2.5.
-
+    double yearsOfService; // Declare the variable 'yearsOfService' of type double
+    yearsOfService = 2.5; // Assign the value 2.5 to the 'yearsOfService' variable
     // Print the yearsOfService variable to the console.
 
+    System.out.println(yearsOfService); // Print the 'yearsOfService' variable to the console
+
     // Create a variable called baseSalary of type int and assign it the value 3000.
+    int baseSalary; // Declare the variable 'baseSalary' of type int
+    baseSalary = 3000; // Assign the value 3000 to the 'baseSalary' variable
 
     // Create a variable called overtimePayment of type int and assign it the value
     // 40.
+    int overtimePayment = 40; // Declare and assign the variable 'overtimePayment' of type int with the value
+                              // 40
 
     // Create a variable called totalPayment of type int and assign it to the value
     // of baseSalary added to overtimePayment.
+    int totalPayment = baseSalary + overtimePayment;
 
     // Print the totalPayment variable to the console.
+    System.out.println(totalPayment);
 
     // Create three variables all of type double on a single line.
     // They should be called firstBonus, secondBonus and thirdBonus and they should
     // be assigned the values 10.00, 22.00 and 35.00.
+    double firstBonus = 10.00, secondBonus = 22.00, thirdBonus = 35.00;
 
     // Print out the sum of the variables called firstBonus, secondBonus and
     // thirdBonus.
+
+    double sum = firstBonus + secondBonus + thirdBonus;
+    System.out.println("Sum of bonuses: " + sum);
 
   }
 
--- _02_06/TicketMachine.java
+++ _02_06/TicketMachine.java
@@ -1,23 +1,60 @@
 package _02_06;
 
 public class TicketMachine {
-  
-public static void main(String[] args) {
-  // Create an object called ticket of type Ticket
 
-  // Set the destination of the ticket to New York
+  public static void main(String[] args) {
+    // Create an object called ticket of type Ticket
+    Ticket ticket = new Ticket();
 
-  // Set the price of the ticket to 15.30
+    // Set the destination of the ticket to New York
+    ticket.setDestination("New York");
 
-  // Set the isReturn value to true
+    // Set the price of the ticket to 15.30
+    ticket.setPrice(15.30);
 
-  // Print the ticket's destination to the console
+    // Set the isReturn value to true
+    ticket.setIsReturn(true);
 
-  // Print the ticket's price to the console
+    // Print the ticket's destination to the console
+    System.out.println("Destination: " + ticket.getDestination());
 
-  // Print the ticket's isReturn value to the console
+    // Print the ticket's price to the console
+    System.out.println("Price: " + ticket.getPrice());
 
+    // Print the ticket's isReturn value to the console
+    System.out.println("Is Return: " + ticket.getIsReturn());
+  }
 }
 
+class Ticket {
+  private String destination;
+  private double price;
+  private boolean isReturn;
 
+  // Getter and setter methods for destination
+  public String getDestination() {
+    return destination;
+  }
+
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  // Getter and setter methods for price
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  // Getter and setter methods for isReturn
+  public boolean getIsReturn() {
+    return isReturn;
+  }
+
+  public void setIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
 }
